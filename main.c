709914c5bb0ce0b08b7f81bb7380569fe6d66fbb
Daniel <danielhumbertoguy@gmail.com>
2021-08-20 23:59:40 -0300

    Commit N°1 del Timer1

---
+++ main.c
@@ -0,0 +1,66 @@
+
+#include <avr/interrupt.h>
+
+//PROTOTIPOS
+
+void  initTimers10ms ();
+void ConfPort (uint8_t  RDireccion,uint8_t  RPuerto,unsigned char PDireccion, unsigned char Direccion , unsigned char PEstado, unsigned char Estado  );
+
+//
+
+// . Variables globales
+volatile  uint16_t timeOut1ms;
+//
+
+
+void ConfPort (uint8_t  RDireccion,uint8_t  RPuerto,unsigned char PDireccion, unsigned char Direccion ,unsigned char PEstado, unsigned char Estado  ) {
+   RDireccion |= (Direccion<< PDireccion);
+   RPuerto |= (Estado << PEstado);
+
+}
+/* ConfPort() es una funcion que permite configurar cualquier puerto del microcontrolador ATMega 328P 
+              RDireccion = es el registro donde se configurar
+			  PDireccion = es la direccion (bit) del Puerto a configurar 
+			  Direccion = Es una variable para configurar el puerto como entrada (1)  o salida (0) 
+	          RPuerto = Es el registro de estado del puerto 
+			  PEstado = Es bit del puerto a configurar
+			  Estado =  Si esta configurado como encendido (1) o apagado (0)
+*/     
+
+void  initTimers10ms () {
+	OCR1A =625 ;    // Valor de compracion 
+	TCNT1 = 0x00 ; // temporizador inicializo
+	TIMSK1 = ( 1 << OCIE1A); // interrupción de compracion en modo CTC OCR1A = TCNT1
+	TCCR1B = ( 1 << WGM12) | ( 1 << CS12) ; // PRESCALAR EN 256 y MODO CTC
+}
+
+
+ISR (TIMER1_COMPA_vect) {
+	/*timeOut1ms--;
+	if (! timeOut1ms) {
+		timeOut1ms = 1000 ;
+		// (1 << PORTB5 1) desplazado por B5 es igual a 00100000
+		if (PORTB & ( 1 << PORTB5)) // desplazo al numero 1 cinco veces para que aparezca en la posicion 5
+		PORTB & = ~ ( 1 << PORTB5); // ~ (1 << PORTB5) = 11011111 hago cero el bit 5 de PORTB5
+		else
+		PORTB | = ( 1 << PORTB5);
+	}*/
+	 PORTB ^= (1<<5);
+	
+}
+	
+	
+
+
+int main(void) {
+
+	sei();
+	initTimers10ms ();
+	ConfPort (DDRB,PORTB,DDB5,1,PORTB5,1);
+	while(1) {
+	
+	
+	}
+
+}
+
