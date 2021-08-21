3777591368fbf356de8bbdf304dbe2c9e54c6064
Daniel <danielhumbertoguy@gmail.com>
2021-08-21 17:05:00 -0300

    Esta andando el timer 1

--- main.c
+++ main.c
@@ -2,38 +2,22 @@
 #include <avr/interrupt.h>
 
 #define F_CPU 16000000L
-
-//PROTOTIPOS
+#define timeEjecucion 25 
+//-----------------PROTOTIPOS--------------------------
 
 void  initTimers10ms ();
-void ConfPort (uint8_t  RDireccion,uint8_t  RPuerto,unsigned char PDireccion, unsigned char Direccion , unsigned char PEstado, unsigned char Estado  );
 void initPortimer();
 
-//
+//------------------------------------------------------
 
-// . Variables globales
+//------------VARIABLES GLOBALES------------------------
 
 volatile  uint16_t timeOut1ms;
 
-//
-
-
-void ConfPort (uint8_t  RDireccion,uint8_t  RPuerto,unsigned char PDireccion, unsigned char Direccion ,unsigned char PEstado, unsigned char Estado  ) {
-   RDireccion |= (Direccion<< PDireccion);
-   RPuerto |= (Estado << PEstado);
-
-}
-/* ConfPort() es una funcion que permite configurar cualquier puerto del microcontrolador ATMega 328P 
-              RDireccion = es el registro donde se configurar
-			  PDireccion = es la direccion (bit) del Puerto a configurar 
-			  Direccion = Es una variable para configurar el puerto como entrada (1)  o salida (0) 
-	          RPuerto = Es el registro de estado del puerto 
-			  PEstado = Es bit del puerto a configurar
-			  Estado =  Si esta configurado como encendido (1) o apagado (0)
-*/     
+//------------------------------------------------------
 
 void  initTimers10ms () {
-	OCR1A =625 ;                            // Valor de compracion 
+	OCR1A =625 ;                            // Valor de compracion para 10 ms
 	TCNT1 = 0x00 ;                          // temporizador inicializo
 	TIMSK1 = ( 1 << OCIE1A);                // interrupción de compracion en modo CTC OCR1A = TCNT1
 	TCCR1B = ( 1 << WGM12) | ( 1 << CS12) ; // PRESCALAR EN 256 y MODO CTC
@@ -41,20 +25,7 @@ void  initTimers10ms () {
 }
 
 
-ISR (TIMER1_COMPA_vect) {
-	/*timeOut1ms--;
-	if (! timeOut1ms) {
-		timeOut1ms = 1000 ;
-		// (1 << PORTB5 1) desplazado por B5 es igual a 00100000
-		if (PORTB & ( 1 << PORTB5)) // desplazo al numero 1 cinco veces para que aparezca en la posicion 5
-		PORTB & = ~ ( 1 << PORTB5); // ~ (1 << PORTB5) = 11011111 hago cero el bit 5 de PORTB5
-		else
-		PORTB | = ( 1 << PORTB5);
-	}*/
-	 PORTB ^= (1<<5);
-	
-}
-	
+
 void initPortimer(){
    DDRB |= (1<<DDB5);
    PORTB |= (1<<PORTB5);
@@ -64,9 +35,9 @@ void initPortimer(){
 
 int main(void) {
     initPortimer();
-	sei();
+	sei();                     //Activa interrupciones en el micro
     initTimers10ms ();
-	
+	timeOut1ms=timeEjecucion ;            // configuro en 250 ms
     while(1) {
 	
 	
@@ -74,3 +45,17 @@ int main(void) {
 
 }
 
+ISR (TIMER1_COMPA_vect) {                           // Interrupcion del timer1
+	timeOut1ms--;
+	if (! timeOut1ms) {
+		timeOut1ms = timeEjecucion ;
+		// (1 << PORTB5 1) desplazado por B5 es igual a 00100000
+		if (PORTB & ( 1 << PORTB5)){ // desplazo al numero 1 cinco veces para que aparezca en la posicion 5
+		PORTB &= ~( 1 << PORTB5);} // ~ (1 << PORTB5) = 11011111 hago cero el bit 5 de PORTB5
+		else{
+		PORTB |= ( 1 << PORTB5);}
+	}
+	 //PORTB ^= (1<<5);
+	
+}
+	
